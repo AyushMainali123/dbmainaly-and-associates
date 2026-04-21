b4ae5f76ce063f37afba056d3aa76e426dd2b036
AyushMainali123 <ayushmainali11@gmail.com>
2026-04-21 19:41:40 +0545

    feat: removed unwanted sections

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,37 @@
 import type { NextConfig } from "next";
 import { withPayload } from '@payloadcms/next/withPayload'
+
+const s3Bucket = process.env.S3_BUCKET;
+const s3Region = process.env.S3_REGION;
+
+const remotePatterns: NonNullable<NextConfig["images"]>["remotePatterns"] = [
+  {
+    protocol: 'http',
+    hostname: 'localhost',
+    port: '3000',
+    pathname: '/api/media/file/**',
+  },
+];
+
+if (s3Bucket && s3Region) {
+  remotePatterns.push({
+    protocol: 'https',
+    hostname: `${s3Bucket}.s3.${s3Region}.amazonaws.com`,
+    pathname: '/**',
+  });
+}
+
+if (s3Region) {
+  remotePatterns.push({
+    protocol: 'https',
+    hostname: `**.s3.${s3Region}.amazonaws.com`,
+    pathname: '/**',
+  });
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '/api/media/file/**',
-      },
-    ],
+    remotePatterns,
   },
 };
 
